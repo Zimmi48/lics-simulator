d9274f529f05f8510f2ab0728cbc2289f1b60201
Théo Zimmermann <theo.zimmermann@ens.fr>
2012-01-05 11:39:44 +0100

    Ajout d'options

--- src/main.ml
+++ src/main.ml
@@ -1,30 +1,44 @@
-let mauvaise_utilisation () =
-  failwith "Utilisation incorrecte : vous devez fournir au simulateur le nom du
-fichier à interpréter, les entrées séparées par des espaces et le nombre
-de cycles à effectuer"
+
+open Printf
+
+(* fichiers d'entrée, options par défaut *)
+let ifile = ref ""
+let cycles = ref 0
+let no_output = ref false
+let rom = ref ""
+let displays = ref 0
+let inputs = ref []
+
+let arguments s =
+let bool_of_string = function
+  | "0" -> false
+  | _ -> true
+in
+  if !ifile = "" then ifile := s
+  else inputs := bool_of_string s :: !inputs
+  
+
+(* liste des options du simulateur *)
+let options = [
+  "cycles", Arg.Set_int cycles, "Nombre de cycles à effectuer";
+  "-no-outputs", Arg.Set no_output, "N'affiche pas les sorties du circuit";
+  "-load-rom", Arg.Set_string rom , "Nom du fichier contenant les valeurs initiales de la ROM";
+  "-displays", Arg.Set_int displays, "Nombre d'afficheurs 7 segments à utiliser"
+]
+
+let () =
+  let usage = Printf.sprintf "Usage : %s [options] <file.bin> <entrees separees par des espaces>" Sys.argv.(0) 
+  in
+  Arg.parse options arguments usage
 
 let lengthArgv = Array.length Sys.argv
 
-let () = if lengthArgv < 3 then mauvaise_utilisation ()
-
-let s = Sys.argv.(1)
-
-let input =
-  let bool_of_int = function
-    | 0 -> false
-    | _ -> true
-  in
-  let rec list_of_args n acc =
-    if n = 1 then acc
-    else list_of_args (n - 1) (bool_of_int (int_of_string Sys.argv.(n)) :: acc)
-  in
-  list_of_args (lengthArgv - 2) []
-
-let n = int_of_string Sys.argv.(lengthArgv - 1)
-
 let () =
   let circuit =
-    try LicsFileIO.read s
-    with _ -> failwith ("Erreur à l'ouverture du fichier " ^ s)
+    try LicsFileIO.read !ifile
+    with _ -> (
+      eprintf "Erreur à l'ouverture du fichier %s\n" !ifile ;
+      exit 1
+    )
   in
-  Simulator.simulator circuit input n
+  Simulator.simulator circuit (List.rev !inputs) !cycles
